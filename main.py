9275768d6382c3bbe19523679294c9e3f096d915
Tim Rudolph <tim.rudolph@s2023.tu-chemnitz.de>
2026-02-01 16:44:31 +0100

    Gliederung

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import qiskit as qi
 import qutip as Q
 import time
@@ -8,24 +9,20 @@ from tensorflow.keras import models, layers, optimizers, losses, metrics, Input
 from tensorflow.keras.utils import plot_model
 from sklearn import metrics as sklearn_metrics
 
+SAMPLES = 500000
 
-# Set the number of samples
-Samples = 500000
+################################### 1 Data ###################################
 
 # Initialize arrays for entangled and separable states
-Entangled = np.empty([Samples, 16], dtype=complex)
-Separable = np.empty([Samples, 16], dtype=complex)
+Entangled = np.empty([SAMPLES, 16], dtype=complex)
+Separable = np.empty([SAMPLES, 16], dtype=complex)
 
 # Initialize arrays for labels
-ent_label = np.empty([Samples, 2], dtype=float)
-sep_label = np.empty([Samples, 2], dtype=float)
+ent_label = np.empty([SAMPLES, 2], dtype=float)
+sep_label = np.empty([SAMPLES, 2], dtype=float)
 
-# Import necessary libraries for file upload and data manipulation
-import pandas as pd
-import numpy as np
-
-# Read the data from a CSV file ('mydata.csv') into a Pandas DataFrame
-data = pd.read_csv('/content/train/mydata.csv')
+# Download from https://www.kaggle.com/datasets/alikookani/quantangle1
+data = pd.read_csv('/data/data.csv')
 
 # Convert the DataFrame to a NumPy array for further processing
 data = np.array(data)
@@ -37,17 +34,17 @@ start = time.time()
 i = 0
 j = 0
 
-while i < Samples or j < Samples:
+while i < SAMPLES or j < SAMPLES:
     Rho = Q.rand_dm(4, dims=[[2, 2], [2, 2]])
     Density = np.array(Rho)
     temp1 = qi.quantum_info.entanglement_of_formation(Density)
 
-    if temp1 == 0.0 and i < Samples:
+    if temp1 == 0.0 and i < SAMPLES:
         Separable[i, :16] = Density.reshape(1, 16)
         sep_label[i, 0] = temp1
         sep_label[i, 1] = np.linalg.det(Q.partial_transpose(Rho, [0, 1]).full()).real
         i += 1
-    elif j < Samples:
+    elif j < SAMPLES:
         Entangled[j, :16] = Density.reshape(1, 16)
         ent_label[j, 0] = temp1
         ent_label[j, 1] = np.linalg.det(Q.partial_transpose(Rho, [0, 1]).full()).real
@@ -77,74 +74,59 @@ tensor1 = matrix.reshape(len(data), 4, 4, 2)
 tensor2 = np.empty([len(data), 4, 4, 2], dtype=float)
 reg_label = np.empty([len(data)], dtype=float)
 class_label = np.ones(len(data)).astype('int')
-for i in range(Samples):
+for i in range(SAMPLES):
     tensor2[2 * i, :, :, :] = tensor1[i, :, :, :]
     reg_label[2 * i] = complex(data[i, 17]).real
     class_label[2 * i] = 0
-    tensor2[2 * i + 1, :, :, :] = tensor1[i + Samples, :, :, :]
-    reg_label[2 * i + 1] = complex(data[i + Samples, 17]).real
+    tensor2[2 * i + 1, :, :, :] = tensor1[i + SAMPLES, :, :, :]
+    reg_label[2 * i + 1] = complex(data[i + SAMPLES, 17]).real
 
-#XpookyNet Model
+################################### 2 Model ###################################
 
 input_tensor = Input(shape=(4, 4, 2))
 layer1 = layers.Conv2D(64, 1)(input_tensor)
-#layer1 = layers.BatchNormalization()(layer1)
 layer1 = layers.LeakyReLU()(layer1)
 layer2 = layers.Conv2D(64, 1)(layer1)
-#layer2 = layers.BatchNormalization()(layer2)
 layer2 = layers.LeakyReLU()(layer2)
 branch_a = layers.Conv2D(256, 4)(layer2)
-#branch_a = layers.BatchNormalization()(branch_a)
 branch_a = layers.LeakyReLU()(branch_a)
 branch_a = layers.Conv2D(128, 1)(branch_a)
-#branch_a = layers.BatchNormalization()(branch_a)
 branch_a = layers.LeakyReLU()(branch_a)
 branch_b = layers.Conv2D(192, 3)(layer2)
-#branch_b = layers.BatchNormalization()(branch_b)
 branch_b = layers.LeakyReLU()(branch_b)
 layer3 = layers.Conv2D(128, 2)(layer2)
-#layer3 = layers.BatchNormalization()(layer3)
 layer3 = layers.LeakyReLU()(layer3)
 layer4 = layers.Conv2D(128, 1)(layer3)
-#layer4 = layers.BatchNormalization()(layer4)
 layer4 = layers.LeakyReLU()(layer4)
 branch_c = layers.Conv2D(96, 3)(layer4)
-#branch_c = layers.BatchNormalization()(branch_c)
 branch_c = layers.LeakyReLU()(branch_c)
 layer5 = layers.Conv2D(128, 2)(layer4)
-#layer5 = layers.BatchNormalization()(layer5)
 layer5 = layers.LeakyReLU()(layer5)
 layer5 = layers.concatenate([layer5, branch_b], axis=-1)
 layer6 = layers.Conv2D(256, 1)(layer5)
-#layer6 = layers.BatchNormalization()(layer6)
 layer6 = layers.LeakyReLU()(layer6)
 layer7 = layers.Conv2D(256, 2)(layer6)
-#layer7 = layers.BatchNormalization()(layer7)
 layer7 = layers.LeakyReLU()(layer7)
 layer7 = layers.concatenate([layer7, branch_a], axis=-1)
 layer8 = layers.Conv2D(512, 1)(layer7)
-#layer8 = layers.BatchNormalization()(layer8)
 layer8 = layers.LeakyReLU()(layer8)
 layer8 = layers.concatenate([layer8, branch_c], axis=-1)
 layer9 = layers.Conv2D(512, 1)(layer8)
-#layer9 = layers.BatchNormalization()(layer9)
 layer9 = layers.LeakyReLU()(layer9)
 layer10 = layers.Conv2D(512, 1)(layer9)
-#layer10 = layers.BatchNormalization()(layer10)
 layer10 = layers.LeakyReLU()(layer10)
 top1 = layers.Flatten()(layer10)
 top1 = layers.Dense(512)(top1)
-#top1 = layers.BatchNormalization()(top1)
 top1 = layers.LeakyReLU()(top1)
-#top2 = layers.Dense(1, name='regression')(top1)
 top3 = layers.Dense(1)(top1)
-#top3 = layers.BatchNormalization()(top3)
 top3 = layers.Activation('sigmoid', name='classification')(top3)
-#model = models.Model(input_tensor, [top2, top3])
 model = models.Model(input_tensor, top3)
+
 model.summary()
 
 plot_model(model, show_shapes=True, to_file='model.png')
+
+################################### 2 Training ###################################
 
 # Compile the model
 callback_list = [
@@ -182,7 +164,7 @@ history1 = model.fit(
                      ),
 )
 
-#First manual reduction in learning rate
+# First manual reduction in learning rate
 model.compile(
     optimizer=optimizers.SGD(
         learning_rate=0.001, momentum=0.9,
@@ -210,7 +192,7 @@ history2 = model.fit(
                      ),
 )
 
-#Second manual reduction in learning rate
+# Second manual reduction in learning rate
 model.compile(
     optimizer=optimizers.SGD(
         learning_rate=0.0001, momentum=0.9,
@@ -238,6 +220,7 @@ history3 = model.fit(
                      ),
 )
 
+################################### 3 Evaluation ###################################
 
 # Load the best saved model
 my_model = tf.keras.models.load_model("my_model.h5")
